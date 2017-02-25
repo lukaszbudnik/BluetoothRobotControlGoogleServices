0e877124e2bd087338208539ab204884ac9fd28f
Łukasz Budnik <lukasz.budnik@gmail.com>
2017-02-25 11:20:52 +0100

    added support for new google services: authentication, database, storage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# BluetoothRobotControl
-A simple Android application written to control my Arduino robots
+# BluetoothRobotControlGoogleServices
+A simple Android application written to control my Arduino robots.
+
+A fork of lukaszbudnik/BluetoothRobotControl with lots of additional Google Services integrations.
--- app/src/main/java/com/github/lukaszbudnik/bluetoothrobotcontrol/RobotControlActivity.java
+++ app/src/main/java/com/github/lukaszbudnik/bluetoothrobotcontrol/RobotControlActivity.java
@@ -10,18 +10,41 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RobotControlActivity extends AppCompatActivity {
 
@@ -29,16 +52,68 @@ public class RobotControlActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 123;
 
+    private static final int RC_SIGN_IN = 456;
+
     private static final List<String> PEER_DEVICES = Arrays.asList("HC-06");
 
     private BluetoothConnectionThread bluetoothConnection;
 
     private VoiceCommandReceiver voiceCommandReceiver;
 
+    private RecordingThread recording;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    private GoogleApiClient googleApiClient;
+
+    private RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_panel);
+
+        requestQueue = Volley.newRequestQueue(this.getApplicationContext());
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    String uid = user.getUid();
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + uid);
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("/users/" + uid + "/settings");
+                    Map<String, String> params = new HashMap<>();
+                    params.put("registrationToken", FirebaseInstanceId.getInstance().getToken());
+                    databaseReference.setValue(params);
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        ToggleButton languageToggleButton = (ToggleButton) findViewById(R.id.language);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.voice_progress);
+        recording = new RecordingThread(progressBar, languageToggleButton, requestQueue);
+        recording.start();
 
         voiceCommandReceiver = new VoiceCommandReceiver();
         IntentFilter intentFilter = new IntentFilter();
@@ -73,8 +148,27 @@ public class RobotControlActivity extends AppCompatActivity {
         }
     }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+            }
+        }
+
         if (requestCode == REQUEST_ENABLE_BT) {
             if (resultCode == Activity.RESULT_OK) {
                 startConnection();
@@ -88,12 +182,47 @@ public class RobotControlActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (firebaseAuthStateListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthStateListener);
+        }
+    }
+
+
+    @Override
     protected void onDestroy() {
         if (bluetoothConnection != null) {
             bluetoothConnection.cancel();
         }
         unregisterReceiver(voiceCommandReceiver);
         super.onDestroy();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     protected void startConnection() {
